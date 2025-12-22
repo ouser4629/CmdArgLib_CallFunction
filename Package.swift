a4834cc3ff25147886187039fc781952550cb2d9
ouser4629 <ouser4629@gmail.com>
2025-12-22 13:14:46 -0500

    Use cmd-arg-lib 0.2.5

--- Package.swift
+++ Package.swift
@@ -27,7 +27,7 @@ let package = Package(
     ],
 
     dependencies: [
-        .package(url: "https://github.com/ouser4629/cmd-arg-lib.git", from: "0.2.4")
+        .package(url: "https://github.com/ouser4629/cmd-arg-lib.git", from: "0.2.5")
     ],
 
     targets: [
