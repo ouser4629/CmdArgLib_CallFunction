14e4e5d117b35bf80fd4f1ffd9b3f1dff9c61377
Peter <p.summerland@gmail.com>
2026-01-26 11:55:45 -0500

    update to 0.3.8

--- Package.swift
+++ Package.swift
@@ -28,7 +28,7 @@ let package = Package(
     ],
 
     dependencies: [
-        .package(url: "https://github.com/ouser4629/cmd-arg-lib.git", from: "0.3.6")
+        .package(url: "https://github.com/ouser4629/cmd-arg-lib.git", from: "0.3.8")
         //        .package(path: "../../\(cmdArgLib)"),
     ],
 
